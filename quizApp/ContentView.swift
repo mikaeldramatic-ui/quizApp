6d97ed0797cb540164508a63393c41a364cc04c1
Micke <mikael.dramatic@gmail.com>
2026-03-27 14:17:33 +0100

    Initial commit quizApp

---
+++ quizApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  quizApp
+//
+//  Created by Mikael Engvall on 2026-03-27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ quizApp/quizAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  quizAppApp.swift
+//  quizApp
+//
+//  Created by Mikael Engvall on 2026-03-27.
+//
+
+import SwiftUI
+
+@main
+struct quizAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
