b75be32af10cfa2a5c49b259bac3f06be8fd2c87
Micke <mikael.dramatic@gmail.com>
2026-03-30 12:09:25 +0200

    Added QuizView with logic and UI

---
+++ quizApp/QuizView.swift
@@ -0,0 +1,90 @@
+//
+//  QuizView.swift
+//  quizApp
+//
+//  Created by Mikael Engvall on 2026-03-29.
+//
+
+import SwiftUI
+
+struct QuizView: View {
+
+    let questions = QuizData.questions // Eller vad Sara har skrivit för något namn
+    
+    @State private var currentQuestionIndex = 0
+    @State private var userAnswer = ""
+    @State private var score = 0
+    
+    var body: some View {
+        
+        ZStack {
+            EmojiBackgroundView(emojis: emojis, isAnimating: false)
+            
+            LinearGradient(
+                gradient: Gradient(colors: [
+                    Color.black.opacity(0.3),
+                    Color.black.opacity(0.5),
+                    Color.black.opacity(0.3)
+                ]),
+                startPoint: .top,
+                endPoint: .bottom
+            )
+            .ignoresSafeArea()
+         
+            //UI quiz
+            VStack(spacing: 30) {
+                Text("Fråga \(currentQuestionIndex + 1) / \(questions.count)")
+                    .font(.headline)
+                    .foregroundColor(.white)
+                
+                Text(questions[currentQuestionIndex].emoji)
+                    .font(.system(size: 80))
+                
+                TextField("Skriv ditt svar...", text: $userAnswer)
+                    .textFieldStyle(.roundedBorder)
+                    .padding(.horizontal)
+                
+                VStack(spacing: 15) {
+                    Button("Nästa") {
+                        checkAnswer()
+                        nextQuestion()
+                    }
+                    .buttonStyle(.borderedProminent)
+                    
+                    Button("Hoppa över") {
+                        nextQuestion()
+                    }
+                    .buttonStyle(.bordered)
+                    
+                    Button("Avbryt") {
+                        print("Quiz avbruten")
+                    }
+                    .foregroundColor(.red)
+                }
+                Spacer()
+            }
+            .padding()
+            
+        }
+    }
+    
+    func checkAnswer() {
+        if userAnswer.lowercased() == questions[currentQuestionIndex].answer.lowercased(){
+            score += 1
+        }
+    }
+    func nextQuestion () {
+        
+        userAnswer = ""
+        
+        if currentQuestionIndex < questions.count - 1 {
+            currentQuestionIndex += 1
+        } else {
+            print ("Quiz klart! Poäng: \(score)")
+        }
+    }
+}
+
+#Preview {
+    QuizView()
+}
